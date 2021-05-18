b93c8807877f24dd27d48f31bccad4f75014d760
PRANTA Dutta <prantadutta1997@gmail.com>
2021-05-18 13:28:02 +0600

    fixed a bug

--- src/hooks/useSessionStorageWithObject.ts
+++ src/hooks/useSessionStorageWithObject.ts
@@ -1,33 +1,5 @@
-import React, { Dispatch, SetStateAction } from 'react'
-
-function useSessionStorage(
-  key: string,
-  initialValue: string = ''
-): [string, Dispatch<SetStateAction<string>>, () => void] {
-  const [item, setValue] = React.useState(() => {
-    const value = sessionStorage.getItem(key) || initialValue
-    sessionStorage.setItem(key, value)
-    return value
-  })
-
-  const setItem = (action: SetStateAction<string>) => {
-    if (action instanceof Function) {
-      return setValue((prevState) => {
-        const value = action(prevState)
-        sessionStorage.setItem(key, value)
-        return value
-      })
-    }
-    setValue(action)
-    sessionStorage.setItem(key, action)
-  }
-
-  const clear = () => {
-    sessionStorage.removeItem(key)
-  }
-
-  return [item, setItem, clear]
-}
+import { Dispatch, SetStateAction } from 'react'
+import { useSessionStorage } from './useSessionStorage'
 
 export function useSessionStorageWithObject<T>(
   key: string,
